67c25c29c5ff96d1c9c50714fc7272736d4fcdab
Ben Mohammad <benmohammad593@gmail.com>
2018-06-16 09:41:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mrmohammad/protalk/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mrmohammad.protalk;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_prof);
+
+
+
+    }
+}
